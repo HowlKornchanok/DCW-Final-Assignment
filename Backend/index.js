d3876cf2c23c198805f32aafbc120ee3d8087262
KORNCHANOK IEDNUSORN (กรชนก เอียดนุสรณ์) <6210110446@email.psu.ac.th>
2022-04-02 23:46:35 +0700

    add Locker

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,13 @@
 const cookieSession = require("cookie-session");
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const express = require("express");
 const cors = require("cors");
 const passportSetup = require("./passport");
 const passport = require("passport");
 const authRoute = require("./routes/auth");
 const app = express();
+
 
 app.use(
   cookieSession({ name: "session", keys: ["Howl"], maxAge: 24 * 60 * 60 * 100 })
@@ -26,3 +29,53 @@ app.use("/auth", authRoute);
 app.listen("5000", () => {
   console.log("Server is running!");
 });
+
+
+'use strict';
+const { createLogger, format, transports } = require('winston');
+require('winston-daily-rotate-file');
+const fs = require('fs');
+const path = require('path');
+
+const env = process.env.NODE_ENV || 'development';
+const logDir = 'log';
+
+// Create the log directory if it does not exist
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
+
+const dailyRotateFileTransport = new transports.DailyRotateFile({
+  filename: `${logDir}/%DATE%-results.log`,
+  datePattern: 'YYYY-MM-DD'
+});
+
+const logger = createLogger({
+  // change level if in dev environment versus production
+  level: env === 'development' ? 'verbose' : 'info',
+  format: format.combine(
+    format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss'
+    }),
+    format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+  ),
+  transports: [
+    new transports.Console({
+      level: 'info',
+      format: format.combine(
+        format.colorize(),
+        format.printf(
+          info => `${info.timestamp} ${info.level}: ${info.message}`
+        )
+      )
+    }),
+    dailyRotateFileTransport
+  ]
+});
+
+logger.error('error message');
+logger.warn('warn message');
+logger.info('info message');
+logger.verbose('verbose message');
+logger.debug('debug message');
+logger.silly('silly message');
